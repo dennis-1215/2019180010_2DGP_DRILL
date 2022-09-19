71307c9c6914dbcf1ae21555ddebb1a7ed613792
dennis-1215 <37973668+dennis-1215@users.noreply.github.com>
2022-09-19 15:38:48 +0900

    drill 05 완성
    2019180010 김정헌

---
+++ 05/drill5.py
@@ -0,0 +1,34 @@
+import turtle
+import random
+
+def up_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def left_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def down_move():
+    turtle.setheading(-90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def right_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.shape("turtle")
+
+turtle.onkey(up_move, 'w')
+turtle.onkey(left_move, 'a')
+turtle.onkey(down_move, 's')
+turtle.onkey(right_move, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
